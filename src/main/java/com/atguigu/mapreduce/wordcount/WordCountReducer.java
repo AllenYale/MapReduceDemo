01484316191914419fcc2e0dec3ddc3d67a52baf
hanyu_LEGION <137728688@qq.com>
2023-06-15 10:06:25 +0800

    hadoop MapReduce WordCount案例 Mapper、Reducer学习梳理

--- src/main/java/com/atguigu/mapreduce/wordcount/WordCountReducer.java
+++ src/main/java/com/atguigu/mapreduce/wordcount/WordCountReducer.java
@@ -14,15 +14,29 @@ import java.io.IOException;
  * @ Date: 2023/6/5 21:43
  * @ Version: v1.0
  */
+
+/**
+ * KEYIN, map的输出是reducer的输入：Text
+ * VALUEIN, IntWritable
+ * KEYOUT,最终输出key类型：Text
+ * VALUEOUT，最终输出Value类型：IntWritable
+ */
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     private IntWritable outV = new IntWritable();
 
+    /**
+     * map阶段切割成：（atguigu，1）（atguigu，1） =》 进入到reduce方法后处理成：（atguigu，（1，1））
+     *
+     * @param key
+     * @param values
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException {
-
         int sum = 0;
-
-        //atguigu, (1,1)
+        //atguigu,(1,1)  values 为（1，1）
         for (IntWritable value : values) {
             sum += value.get();
         }
